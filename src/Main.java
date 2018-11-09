ffa3a9954f567beecc0fab450ed5518d93d63383
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2018-11-09 08:28:20 +0800

    add sum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     // todo
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); // comment
+    }
+
+    // ctl + alt + shift + T        refactor this
+    // alt + `                      vcs operations
+    // alt + 9                      version control view
+    public int sum() {
+        int i = 3;
+        return i;
     }
 }
